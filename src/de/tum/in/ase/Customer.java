c787f302e29b87ea322dc7222af263b058558aa1
MetehanOner <a.metehanoner@gmail.com>
2022-12-12 22:50:22 +0100

    last commit of hard work++ private implemented everything

--- src/de/tum/in/ase/Customer.java
+++ src/de/tum/in/ase/Customer.java
@@ -48,7 +48,9 @@ public class Customer {
 
     public void placeAllProductsOnBand(Queue<Product> band){
 
-        for(int i = productsInBasket.size(); i == 0; i--){
+        int corner = productsInBasket.size();
+
+        for(int i = 0; i < corner; i++){
 
             band.enqueue(productsInBasket.pop());
 
@@ -57,6 +59,8 @@ public class Customer {
     }
 
     public void takeAllProductsFromBand(Queue<Product> band){
+
+        int corner = band.size();
 
         for(int i = 0; i < band.size(); i++){
 
@@ -97,5 +101,28 @@ public class Customer {
 
     public Stack<Product> getProductsInBasket() {
         return productsInBasket;
+    }
+
+    public static void main(String[] args) {
+        Product p1 = new Product("yağ", 1000);
+        Product p2 = new Product("yağ", 100);
+        Product p3 = new Product("yağ", 20);
+        Product p4 = new Product("yağ", 50);
+        Product p5 = new Product("yağ", 60);
+
+        Customer customer = new Customer("mete", 2000);
+
+        customer.addProductToBasket(p1);
+        customer.addProductToBasket(p2);
+        customer.addProductToBasket(p3);
+        customer.addProductToBasket(p4);
+        customer.addProductToBasket(p5);
+
+        Checkout c1 = new Checkout();
+        Checkout c2 = new Checkout();
+        Checkout c3 = new Checkout();
+
+        customer.placeAllProductsOnBand(c1.getBandBeforeCashier());
+
     }
 }
