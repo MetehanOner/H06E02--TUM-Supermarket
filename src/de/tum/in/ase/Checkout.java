5b5893e6c37e4592cdee33eeaeb2de738a56c4f4
MetehanOner <a.metehanoner@gmail.com>
2022-12-13 11:10:08 +0100

    checkout implemented

--- src/de/tum/in/ase/Checkout.java
+++ src/de/tum/in/ase/Checkout.java
@@ -18,32 +18,26 @@ public class Checkout {
         return customers.size();
     }
 
-    public void serveNextCustomer(){
+    public void serveNextCustomer() {
 
         if(customers.isEmpty()) {
             throw new UnsupportedOperationException();
         }
 
-        Customer nCustomer = customers.dequeue();
+        Customer currentCustomer = customers.dequeue();
 
-        nCustomer.placeAllProductsOnBand(bandBeforeCashier);
+        currentCustomer.placeAllProductsOnBand(bandBeforeCashier);
 
-        /*for(int i = nCustomer.getProductsInBasket().size(); i == 0; i--){
-
-            bandBeforeCashier.enqueue(nCustomer.getProductsInBasket().pop());
-
-        }*/
-
-        double sum = 0;
-        for(int i = 0; i < bandBeforeCashier.size(); i++){
-
-            Product next = bandBeforeCashier.dequeue();
-            sum = sum + next.getPrice();
-            bandAfterCashier.enqueue(next);
+        double sum=0;
+        int corner = bandBeforeCashier.size();
+        for(int i=0; i<corner; i++) {
+            Product temp = bandBeforeCashier.dequeue();
+            sum = sum + temp.getPrice();
+            bandAfterCashier.enqueue(temp);
         }
 
-        nCustomer.takeAllProductsFromBand(bandAfterCashier);
-        nCustomer.pay(sum);
+        currentCustomer.pay(sum);
+        currentCustomer.takeAllProductsFromBand(bandAfterCashier);
 
     }
 
@@ -58,4 +52,38 @@ public class Checkout {
     public Queue<Product> getBandBeforeCashier() {
         return bandBeforeCashier;
     }
+
+    /*public static void main(String[] args) {
+        Customer c0 = new Customer("feli", 1500);
+        Customer c1 = new Customer("mete", 1750);
+        Customer c2 = new Customer("freiheit", 5000);
+
+        Product p1 = new Product("yağ", 1000);
+        Product p2 = new Product("yağ", 100);
+        Product p3 = new Product("yağ", 20);
+        Product p4 = new Product("yağ", 50);
+        Product p5 = new Product("yağ", 60);
+
+        c0.addProductToBasket(p1);
+        c0.addProductToBasket(p2);
+        c0.addProductToBasket(p3);
+
+        c1.addProductToBasket(p3);
+        c1.addProductToBasket(p4);
+        c1.addProductToBasket(p5);
+
+        c2.addProductToBasket(p1);
+        c2.addProductToBasket(p4);
+        c2.addProductToBasket(p2);
+
+        Checkout checkout = new Checkout();
+        checkout.customers.enqueue(c0);
+        checkout.customers.enqueue(c1);
+        checkout.customers.enqueue(c2);
+
+        checkout.serveNextCustomer();
+        checkout.serveNextCustomer();
+        checkout.serveNextCustomer();
+
+    }*/
 }
